6b291cb135b32f6b03eb92576413b667b3e2128c
Dan Truong <dan.truong@gmail.com>
2019-10-30 15:45:18 -0700

    Added 255 as max speed, and more menu options

--- 1_PWM_Servomotor_Range_Scan/1_PWM_Servomotor_Range_Scan.ino
+++ 1_PWM_Servomotor_Range_Scan/1_PWM_Servomotor_Range_Scan.ino
@@ -53,26 +53,63 @@
 #include <Adafruit_PWMServoDriver.h>
 
 // Verbose printing to debug the code only
-#define DEBUG_PRINT(t)
-//#define DEBUG_PRINT(t) Serial.print(t)
+//#define DEBUG_PRINT(t)
+#define DEBUG_PRINT(t) Serial.print(t)
+#define DEBUG_PRINT_MAX(t)
 
 // Instantiate the PWM motor driver at address 0x40
 Adafruit_PWMServoDriver servoMotors = Adafruit_PWMServoDriver(0x40);
 
-// You can lower this value if there's less than 8 motors
-constexpr uint8_t numServoMotors = 8;
 
 // Limits for each motor
-uint16_t servoMotorsMin[8] = {240, 240, 240, 240, 240, 240, 240, 240};
-uint16_t servoMotorsMax[8] = {260, 260, 260, 260, 260, 260, 260, 260};
+#define TARGET_BOARD 2
+
+#if (TARGET_BOARD == 1)
+// Actual values for the Barbary costume head
+constexpr uint8_t numServoMotors = 6; // Motors 3&4 are unused.
+uint16_t servoMotorsMin[numServoMotors] =       {150, 220, 200, 485, 250, 485};
+uint16_t servoMotorsMax[numServoMotors] =       {400, 280, 140, 240, 250, 240};
+uint16_t servoMotorsPosition[numServoMotors] =  {250, 250, 250, 250, 250, 250};
+uint8_t  servoMotorsWait[numServoMotors] =      {0, 0, 0, 0, 0, 0};
+int8_t   servoMotorsDirection[numServoMotors] = {1, 1, 1, 1, 1, 1};
+bool     servoMotorsOn[numServoMotors] =        {false, false, false, false, false, false};
+uint8_t  servoMotorStepDelay = 5;
+#pragma message "Target: BARBARY costume  "
+
+#elif (TARGET_BOARD == 2)
+constexpr uint8_t numServoMotors = 4;
+uint16_t servoMotorsMin[numServoMotors] =       {190, 210, 210, 190};
+uint16_t servoMotorsMax[numServoMotors] =       {500, 450, 450, 500};
+uint16_t servoMotorsPosition[numServoMotors] =  {250, 250, 250, 250};
+uint8_t  servoMotorsWait[numServoMotors] =      {0, 0, 0, 0,};
+int8_t   servoMotorsDirection[numServoMotors] = {1, 1, 1, 1};
+bool     servoMotorsOn[numServoMotors] =        {false, false, false, false};
+uint8_t  servoMotorStepDelay = 5;
+#pragma message "Target: barbary PROTOTYPE"
+
+#else
+// You can lower this value if there's less than 8 motors
+constexpr uint8_t numServoMotors = 8;
+//       Min/max positions of the motors (what we're trying to find with this program)
+uint16_t servoMotorsMin[numServoMotors] =       {240, 240, 240, 240, 240, 240, 240, 240};
+uint16_t servoMotorsMax[numServoMotors] =       {260, 260, 260, 260, 260, 260, 260, 260};
+//       Current position of the motor (as is known by the program)
+uint16_t servoMotorsPosition[numServoMotors] =  {250, 250, 250, 250, 250, 250, 250, 250};
+//       Time left for a motor to wait before moving again at the end of a move
+uint8_t  servoMotorsWait[numServoMotors] =      {0, 0, 0, 0, 0, 0, 0, 0};
+//       Direction of movement, +1:min-> max, -1:max->min
+int8_t   servoMotorsDirection[numServoMotors] = {1, 1, 1, 1, 1, 1, 1, 1};
+//       Is the motor enabled to move
+bool     servoMotorsOn[numServoMotors] =        {false, false, false, false, false, false, false, false};
+// How long to wait between increments? 255 = no wait between min/max
+uint8_t  servoMotorStepDelay = 5;
+
+#endif
+
+// The Feather supports up to 8 motors (0..7)
 static_assert(numServoMotors <= 8);
-// Current motor position, direction, and wait (if the end of a motion)
-bool servoMotorsOn[8] = {false, false, false, false, false, false, false, false};
-uint16_t servoMotorsPosition[8] = {250, 250, 250, 250, 250, 250, 250, 250};
-int8_t servoMotorsDirection[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-uint16_t servoMotorsWait[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
 // Delay between each step of the motor (bigger is slower, in msec)
-uint8_t servoMotorStepDelay = 5;
 // Motor delay at the end of a movement, before changing direction (msec)
 constexpr uint16_t servoMotorEndDelay = 1000;
 
@@ -82,6 +119,8 @@ constexpr uint16_t servoMotorEndDelay = 1000;
 ///////////////////////////////////////////////////////////////////////////////
 void setup()
 {
+  delay(1000);  
+
   // Initialize the servo motors at a 60Hz PWM frequency.
   servoMotors.begin();
   servoMotors.setPWMFreq(60);
@@ -92,6 +131,18 @@ void setup()
   while (!Serial){};
   Serial.println(VERSION);
   Serial.println(COPYRIGHT);
+  delay(1000);  
+
+  // Initialize the motor positions
+  Serial.println("Initializing motors...");
+  for (uint8_t m = 0; m < numServoMotors; m++)
+  {
+    Serial.println(m);
+    servoMotors.setPWM(m, 0, servoMotorsMax[m]);
+    delay(100);  
+    servoMotors.setPWM(m, 0, servoMotorsPosition[m]);
+    delay(1000);  
+  }
   Serial.println("Ready!");
 
   showMenu();
@@ -119,42 +170,52 @@ void loop()
       --servoMotorsWait[m];
       if (servoMotorsWait[m] == 0)
       {
+        DEBUG_PRINT(m);
         DEBUG_PRINT("WAIT ");
       }
       continue;
     }
     // Update the motor's position then move it to the new position
-    servoMotorsPosition[m] += servoMotorsDirection[m];
-    servoMotors.setPWM(m, 0, servoMotorsPosition[m]);
-    DEBUG_PRINT(servoMotorsPosition[m]);
-    DEBUG_PRINT("\n");
-
-    // Verify end of movement conditions
-    if (servoMotorsPosition[m] >= servoMotorsMax[m])
+    // If speed is 255, then just move min to max, no steps, but also
+    // motors are not moving at the same time.
+    if (servoMotorStepDelay != 255)
+    {
+      servoMotorsPosition[m] += servoMotorsDirection[m];
+    } else if (servoMotorsDirection[m] == 1)
     {
       servoMotorsPosition[m] = servoMotorsMax[m];
-      servoMotorsDirection[m] = -1;
-      Serial.print("M");
-    }
-    if (servoMotorsPosition[m] <= servoMotorsMin[m])
+    } else
     {
       servoMotorsPosition[m] = servoMotorsMin[m];
-      servoMotorsDirection[m] = 1;
-      // We must convert the wait in loop iterations
-      Serial.print("m");
     }
+    
+    // End of movement conditions
     if ((servoMotorsPosition[m] >= servoMotorsMax[m]) ||
         (servoMotorsPosition[m] <= servoMotorsMin[m]))
     {
+      // Convert the wait (msec) into loop iterations
       servoMotorsWait[m] = servoMotorEndDelay / servoMotorStepDelay;
-      Serial.print(m);
-      Serial.print("=");
-      Serial.print(servoMotorsPosition[m]);
+
+      // Enforce limits and change direction
       if (servoMotorsPosition[m] >= servoMotorsMax[m])
-        Serial.print("\n");
-      else
-        Serial.print("\n");
+      {
+        servoMotorsPosition[m] = servoMotorsMax[m];
+        servoMotorsDirection[m] = -1;
+        DEBUG_PRINT("M");
+      }
+      else if (servoMotorsPosition[m] <= servoMotorsMin[m])
+      {
+        servoMotorsPosition[m] = servoMotorsMin[m];
+        servoMotorsDirection[m] = 1;
+        DEBUG_PRINT("m");
+      }
+      DEBUG_PRINT(m);
+      DEBUG_PRINT("=");
+      DEBUG_PRINT(servoMotorsPosition[m]);
+      DEBUG_PRINT("\n");
     }
+
+    servoMotors.setPWM(m, 0, servoMotorsPosition[m]);
   } // for
 
   // Wait a millisecond at the end of the loop
@@ -189,9 +250,11 @@ void showMenu()
   Serial.println("2 - Set current Servo Motor Min value");
   Serial.println("3 - Set current Servo Motor Max value");
   Serial.println("4 - Turn on current Servo Motor");
-  Serial.println("5 - Turn off all Servo Motors");
-  Serial.println("6 - Set Servo Motors delay");
-  Serial.println("7 - Show Servo Motors status");
+  Serial.println("5 - Turn on all Servo Motors");
+  Serial.println("6 - Center all Servo Motors");
+  Serial.println("7 - Turn off all Servo Motors");
+  Serial.println("8 - Set Servo Motors delay");
+  Serial.println("9 - Show Servo Motors status");
   Serial.println("=====================================\n");
 }
 
@@ -218,17 +281,17 @@ void menu()
     if (character >= '0' && character <= '9')
     {
       input = input * 10 + (character - '0');
-      DEBUG_PRINT("[");
-      DEBUG_PRINT(input);
-      DEBUG_PRINT("]");
+      DEBUG_PRINT_MAX("[");
+      DEBUG_PRINT_MAX(input);
+      DEBUG_PRINT_MAX("]");
     }
     else
     {
-      DEBUG_PRINT(".");
+      DEBUG_PRINT_MAX(".");
     }
     delay(1); // Needed to capture whole input line
   } while (Serial.available());
-  DEBUG_PRINT("~ ");
+  DEBUG_PRINT_MAX("~ ");
 
   // Choosing an option from the main showMenu() menu
   if (readMainMenu)
@@ -294,6 +357,10 @@ void menu()
       Serial.print("): ");
       readMainMenu = false;
     }
+    else if (input > servoMotorsMax[selectedServoMotor])
+    {
+       Serial.println("NOT set! (value too high)");     
+    }
     else
     {
       // read the value.
@@ -320,6 +387,10 @@ void menu()
       Serial.print(servoMotorsMax[selectedServoMotor]);
       Serial.print("): ");
       readMainMenu = false;
+    }
+    else if (input < servoMotorsMin[selectedServoMotor])
+    {
+      Serial.println("NOT set! (value too low)");
     }
     else
     {
@@ -351,25 +422,58 @@ void menu()
     }
     break;
 
-  // Set servoMotorsOn[*]=false
+  // Set servoMotorsOn[*]=true
   case 5:
     for (uint8_t m = 0; m < numServoMotors; m++)
     {
       Serial.print("Motor ");
-      Serial.print(selectedServoMotor);
-      if (servoMotorsOn[selectedServoMotor])
+      Serial.print(m);
+      if (servoMotorsOn[m])
       {
         Serial.print(" is already");
       }
       Serial.println(" off!");
-      servoMotorsOn[selectedServoMotor] = false;
+      servoMotorsOn[m] = true;
+    }
+    Serial.println("");
+    showMenu();
+    break;
+
+  // Set servoMotorsOn[*]=true
+  case 6:
+    for (uint8_t m = 0; m < numServoMotors; m++)
+    {
+      Serial.print("Motor ");
+      Serial.print(m);
+      Serial.println(" centered!");
+      uint16_t center = servoMotorsMin[m] +
+                        (servoMotorsMax[m] - servoMotorsMin[m])/2;
+      servoMotorsPosition[m] = center;
+      servoMotors.setPWM(m, 0, center);
+    }
+    Serial.println("");
+    showMenu();
+    break;
+
+  // Set servoMotorsOn[*]=false
+  case 7:
+    for (uint8_t m = 0; m < numServoMotors; m++)
+    {
+      Serial.print("Motor ");
+      Serial.print(m);
+      if (servoMotorsOn[m])
+      {
+        Serial.print(" is already");
+      }
+      Serial.println(" off!");
+      servoMotorsOn[m] = false;
     }
     Serial.println("");
     showMenu();
     break;
 
   // Set servoMotorStepDelay
-  case 6:
+  case 8:
     if (readMainMenu)
     {
       // prepare to read the value.
@@ -388,7 +492,7 @@ void menu()
     }
     break;
 
-  case 7:
+  case 9:
     for (uint8_t m = 0; m < numServoMotors; m++)
     {
       Serial.print("Motor");
@@ -423,10 +527,10 @@ void menu()
     Serial.println("!");
     break;
   } // switch
-  DEBUG_PRINT(" -");
-  DEBUG_PRINT(readMainMenu);
-  DEBUG_PRINT("-");
-  DEBUG_PRINT(menuChoice);
-  DEBUG_PRINT("-> ");
-  DEBUG_PRINT(input);
+  DEBUG_PRINT_MAX(" -");
+  DEBUG_PRINT_MAX(readMainMenu);
+  DEBUG_PRINT_MAX("-");
+  DEBUG_PRINT_MAX(menuChoice);
+  DEBUG_PRINT_MAX("-> ");
+  DEBUG_PRINT_MAX(input);
 }
